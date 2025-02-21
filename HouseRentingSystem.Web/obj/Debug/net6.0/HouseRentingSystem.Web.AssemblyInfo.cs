47205d19e77a970f651a3b556414ef375f7202f6
maya-ivanova <maya.ivanova@arabis-bg.org>
2025-02-22 01:43:03 +0200

    branch syntax final check

--- HouseRentingSystem.Web/obj/Debug/net6.0/HouseRentingSystem.Web.AssemblyInfo.cs
+++ HouseRentingSystem.Web/obj/Debug/net6.0/HouseRentingSystem.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRentingSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb8715368c066eee3fdd42e483c619966ca0b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c79b27d1e28a04d7201968ade95c88c55d66e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRentingSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRentingSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,6 @@
 node {
         stage('Define env variables'){
-        env.GIT_BRANCH = 'origin/feature-ci-pipeline' 
+        env.GIT_BRANCH = 'refs/heads/feature-ci-pipeline' 
         env.PATH_TO_PROJECT_FILE = "C:\\Users\\jenkins\\Documents\\SEDO-Regular-Exam-2024-10\\HouseRentingSystem.sln"
         }
 
@@ -11,11 +11,6 @@ node {
         stage('Restore Project Packages') {
             bat "dotnet restore %PATH_TO_PROJECT_FILE%"
             echo "Package restore done!"
-        }
-
-        stage('Build the Project') {
-            bat "dotnet build %PATH_TO_PROJECT_FILE%"
-            echo "Build done!"
         }
 
         stage('Add final line') {
